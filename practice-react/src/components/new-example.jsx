d366717425cffb39b4e15dac47703a727e816b9f
Vandana Khaparde <vandana.khaparde@gmail.com>
2020-03-26 01:35:17 +0000

    examples

--- practice-react/src/components/new-example.jsx
+++ practice-react/src/components/new-example.jsx
@@ -9,9 +9,42 @@ export default class NewExample extends React.Component{
       firstName : "Harper",
       lastName : "Perez"
     };
+
     const element = (
-    <h1 tabIndex="100">Hello, {formatName(name)}</h1>
+    <div>
+        <h1 tabIndex="100">Hello, {formatName(name)}</h1>
+        <h2>It is {new Date().toLocaleTimeString()}</h2>
+    </div>
     );
-    return element;
+    return (
+      <div>
+        {element}
+        <Welcome name="Ajit Khaparde"/>
+        <Comment date= {comment.date} text = {comment.text} author = {comment.author}/>
+      </div>
+    );
   }
+}
+function Welcome(props){
+  return <h1>Welcome, {props.name}</h1>
+}
+const comment = {
+  date : new Date().toLocaleTimeString(),
+  text: "I love learning React",
+  author : {
+    name: "Harper Perez",
+    url:"https://previews.123rf.com/images/almoond/almoond1502/almoond150200035/36944963-spring-background-with-blossom-branch-of-pink-flowers-vector.jpg"
+  }
+};
+function Comment(props){
+return (
+  <div>
+    <div>
+      <img src = {props.author.url} height="100px"></img>
+      <div>Hello {props.author.name}</div>
+      <div>{props.date}</div>
+      <div>{props.text}</div>
+    </div>
+  </div>
+);
 }
