19fd70bb73cfd62fd768cb8807c6fd7139d38f53
Vandana Khaparde <vandana.khaparde@gmail.com>
2020-03-28 23:43:51 +0000

    event handling examples

--- practice-react/src/components/app.jsx
+++ practice-react/src/components/app.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Clock from './clock';
 import NewExample from './new-example';
+import ToggleButton from './toggle-button';
 
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
-      <Clock/>
-      <NewExample/>
+      {/* <Clock/> */}
+      {/* <NewExample/> */}
+      <ToggleButton/>
       </div>
    );
   }
---
+++ practice-react/src/components/toggle-button.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export default class ToggleButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      toggle: false,
+      text: "OFF"
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick(event) {
+    event.preventDefault();
+    this.setState({
+      toggle: !this.state.toggle,
+       text : this.state.toggle ? "ON" : "OFF"});
+  }
+  render() {
+      return (
+      <button onClick={this.handleClick} style = {{backgroundColor:"red"}}>{this.state.text}
+        {/* { this.state.toggle ? "ON" : "OFF"} */}
+      </button>
+      );
+  }
+}
