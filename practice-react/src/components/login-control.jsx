9463b078b4710ee7396465939ca992daee4a159b
Vandana Khaparde <vandana.khaparde@gmail.com>
2020-04-01 20:15:15 +0000

    Conditional rendering

--- practice-react/src/components/login-control.jsx
+++ practice-react/src/components/login-control.jsx
@@ -1,26 +1,51 @@
 import React from 'react';
 
-export default class LoginControl extends React.Component{
-  constructor(props){
+export default class LoginControl extends React.Component {
+  constructor(props) {
     super(props);
     this.state = {
-      isLoggedIn : false
+      isLoggedIn: false
     }
+    this.handleClickLogIn = this.handleClickLogIn.bind(this);
+    this.handleClickLogOut = this.handleClickLogOut.bind(this);
+  }
+  handleClickLogIn(){
+    this.setState({ isLoggedIn: true });
+  }
+  handleClickLogOut(){
+    this.setState({isLoggedIn : false});
   }
   render(){
-    const isLoggedIn = this.state.isLoggedIn;
-    let button ;
-    if(isLoggedIn){
-      button = <button onClick = {()=>{
-        this.setState({isLoggedIn:true});
-      }
-      }>Log Out</button>
+    let isLoggedIn = this.state.isLoggedIn;
+    console.log('isLoggedIn',isLoggedIn);
+    let button;
+    if (isLoggedIn) {
+      button = <LogOutButton onClick = {this.handleClickLogOut}/>
     }
-    button = <button onClick = {()=>{
-      this.setState({isLoggedIn:false})
-    }}>Log In</button>
+    else {
+      button = <LogInButton onClick ={this.handleClickLogIn}/>
+    }
+    return (
+      <div>
+        <UserGreeting isLoggedIn={isLoggedIn}/>
+        {button}
+      </div>
+    );
   }
 }
-function UserGreeting(props){
 
+function UserGreeting(props) {
+  if (props.isLoggedIn) {
+    return <h1>Welcome User</h1>
+  }
+  else{
+    return <h1>Please Log In</h1>
+  }
+}
+
+function LogInButton(props){
+  return <button onClick = {props.onClick}>Log In</button>
+}
+function LogOutButton(props){
+  return <button onClick= {props.onClick}>Log Out</button>
 }
