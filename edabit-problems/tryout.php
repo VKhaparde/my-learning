a7312359237ec453e9d650e15b127b22511367cd
Vandana Khaparde <vandana.khaparde@gmail.com>
2020-04-04 05:15:40 +0000

    Learning Php

--- edabit-problems/tryout.php
+++ edabit-problems/tryout.php
@@ -1,18 +1,51 @@
 <?php
-    $total = "25 students";
-    $more = 10;
-    $total = $total + $more;
-    echo "$total";
+//     $total = "25 students";
+//     $more = 10;
+//     $total = $total + $more;
+//     echo "$total";
 
-$foo = 'Bob';
-$bar = &$foo;
-$bar = "My name is $bar";
-echo $bar;
-echo $foo;
+// $foo = 'Bob';
+// $bar = &$foo;
+// $bar = "My name is $bar";
+// echo $bar;
+// echo $foo;
 
-$a = array("A", "Cat", "Dog", "A", "Dog");
-$b = array("A", "A", "Cat", "A", "Tiger");
-$c = array_combine($a, $b);
-echo $c;
-print_r(array_count_values($c));
+// $a = array("A", "Cat", "Dog", "A", "Dog");
+// $b = array("A", "A", "Cat", "A", "Tiger");
+// $c = array_combine($a, $b);
+// echo $c;
+// print_r(array_count_values($c));
+session_start();
+$count  =0;
+$_SESSION['key1'] =2;
+$_SESSION['key2']=4;
+session_destroy();
+$count = count($_SESSION);
+echo $count;
+
+$a = 1;
+++$a;
+$a *= $a;
+echo $a--;
+
+// class Foo{
+//   protected $_value;
+//   public function __construct()
+//   {
+//     $this->_value = 1;
+//   }
+//   public static function getSomething(){
+//     return $this->_value * 5;
+//   }
+// }
+// echo Foo::getSomething();
+$w = 'hello';
+echo  "$w,world";
+echo $w.',world';
+
+$var = '0';
+var_dump(isset($var));
+var_dump(empty($var));
+var_dump(is_null($var));
+
 ?>
