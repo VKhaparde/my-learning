515ba71d3809f6d384022d47c6d9a6b8da26c56a
Vandana Khaparde <vandana.khaparde@gmail.com>
2020-05-01 23:13:49 +0000

    React Chronometer build

--- practice-react/src/components/chronometer.jsx
+++ practice-react/src/components/chronometer.jsx
@@ -11,33 +11,41 @@ export default class Chronometer extends React.Component {
       timerTime: 0
       //total time the timer has been running since start or timer has resumed
     };
+    this.startTimer = this.startTimer.bind(this);
+    this.stopTimer = this.stopTimer.bind(this);
+    this.resetTimer = this.resetTimer.bind(this);
   }
-  startTimer = () => {
+  startTimer() {
     this.setState({
       timerOn: true,
       timerTime: this.state.timerTime,
       timerStart: Date.now() - this.state.timerTime
     });
+    console.log('timerStart',this.state.timerStart);
     this.timer = setInterval(() => {
       this.setState({
         timerTime: Date.now() - this.state.timerStart
       });
     }, 10);
+    console.log('timerStart', this.state.timerStart);
+    console.log('timerTime', this.state.timerTime);
+
   };
 
-  stopTimer = () => {
+  stopTimer() {
     this.setState({ timerOn: false });
     clearInterval(this.timer);
   };
-  resetTimer = () => {
+  resetTimer() {
     this.setState({
       timerStart: 0,
       timerTime: 0
     });
   };
   render() {
-    console.log(Date.now());
+    console.log("Date.now()", Date.now());
     const { timerTime } = this.state;
+    console.log('timerTime', timerTime);
     let centiseconds = ("0" + (Math.floor(timerTime / 10) % 100)).slice(-2);
     let seconds = ("0" + (Math.floor(timerTime / 1000) % 60)).slice(-2);
     let minutes = ("0" + (Math.floor(timerTime / 60000) % 60)).slice(-2);
