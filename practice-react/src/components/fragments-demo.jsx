89689a621bed87c2b0d7779eb506229429ec5128
Vandana Khaparde <vandana.khaparde@gmail.com>
2020-05-06 22:18:17 +0000

    Practicing React Advanced Context and Hooks

--- practice-react/src/components/fragments-demo.jsx
+++ practice-react/src/components/fragments-demo.jsx
@@ -3,25 +3,32 @@ import React, { Fragment } from 'react';
 export default function FragmentsDemo(props) {
   console.log('props in Fragments Demo', props);
   return (
-    <dl>
-      {
-        props.posts.map((currentVal)=>{
-        <ListItem key={currentVal.id} posts={currentVal} />
-        })
-      }
-    </dl>
+    <div>
+      Hello React Fragments!!!
+      <dl>
+        {
+          props.posts.map((currentVal) => {
+            // <ListItem key={currentVal.id} posts={currentVal} />
+            <React.Fragment key={currentVal.id}>
+              <dt>{currentVal.title}</dt>
+              <dd>{currentVal.text}</dd>
+            </React.Fragment>
+          })
+        }
+      </dl>
+    </div>
   );
 
 }
 
-function ListItem({posts}) {
-  console.log('Props in ListItem', {posts});
-    return (
-      <React.Fragment >
-        <dt>{posts.title}</dt>
-        <dd>{posts.text}</dd>
-      </React.Fragment>
-    )
+// function ListItem({posts}) {
+//   console.log('Props in ListItem', {posts});
+//     return (
+//       <React.Fragment >
+//         <dt>{posts.title}</dt>
+//         <dd>{posts.text}</dd>
+//       </React.Fragment>
+//     )
 
 
-}
+// }
