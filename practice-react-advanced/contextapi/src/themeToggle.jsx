b0814e746ea72677a91e61badbd8a3d154aa1283
Vandana Khaparde <vandana.khaparde@gmail.com>
2020-05-08 17:13:13 +0000

    Converting all class components to functional to seee the usage of useContext() hook

--- practice-react-advanced/contextapi/src/themeToggle.jsx
+++ practice-react-advanced/contextapi/src/themeToggle.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import { ThemeContext } from './contexts/ThemeContext';
+import { useContext } from 'react';
 
-export default class ThemeToggle extends React.Component{
-  static contextType = ThemeContext;
+// export default class ThemeToggle extends React.Component{
+//   static contextType = ThemeContext;
 
-  render(){
-    const {toggleTheme} = this.context;
-    return(
-      <button onClick={toggleTheme}>
-        Toggle Theme
+//   render(){
+//     const {toggleTheme} = this.context;
+//     return(
+//       <button onClick={toggleTheme}>
+//         Toggle Theme
+//     </button>
+//     );
+//   }
+// }
+
+// Converting ThemeToggle to functional component to see the usage of useContext() hook
+const ThemeToggle = ()=>{
+  const {toggleTheme} = useContext(ThemeContext);
+  return(
+    <button onClick={toggleTheme}>
+      Toggle Theme
     </button>
-    );
-  }
+  );
 }
-
-const
+export default ThemeToggle;
