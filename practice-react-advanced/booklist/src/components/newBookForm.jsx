ca33043484f6267da45617342e249d0db12d3f3a
Vandana Khaparde <vandana.khaparde@gmail.com>
2020-05-11 22:14:01 +0000

    reducers

--- practice-react-advanced/booklist/src/components/newBookForm.jsx
+++ practice-react-advanced/booklist/src/components/newBookForm.jsx
@@ -1,25 +1,25 @@
-import React, {useState , useContext} from 'react';
+import React, { useState, useContext } from 'react';
 import { BookContext } from '../contexts/BookContext';
 
-const NewBookForm = ()=>{
+const NewBookForm = () => {
   const { addBook } = useContext(BookContext);
- const [title,setTitle] =useState('');
- const [author,setAuthor] = useState('');
- const handleSubmit = (event) =>{
-     event.preventDefault();
-     addBook(title, author);
-     setTitle('');
-     setAuthor('');
- }
-  return(
-    <form onSubmit = {handleSubmit}>
-      <input type="text" placeholder = "book title" onChange = {(event)=>{
-          setTitle(event.target.value);
-      }}/>
-      <input type ="text" placeholder = "author" onChange = {(event)=>{
-          setAuthor(event.target.value);
-      }}/>
-      <input type="submit" value="Add book"/>
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    addBook(title, author);
+    setTitle('');
+    setAuthor('');
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" placeholder="book title" required onChange={(event) => {
+        setTitle(event.target.value);
+      }} />
+      <input type="text" placeholder="author" required onChange={(event) => {
+        setAuthor(event.target.value);
+      }} />
+      <input type="submit" value="Add book" />
     </form>
   );
 }
