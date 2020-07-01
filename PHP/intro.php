bee06f12cc374a27c818979a34256e0219e32ebe
Vandana Khaparde <vandana.khaparde@gmail.com>
2020-07-01 23:59:39 +0000

    revising variable decalaration

--- PHP/intro.php
+++ PHP/intro.php
@@ -3,8 +3,26 @@
   //THis is comment
   # This is also comment
   /* Multiple comments  */
-  
+
   Echo "Hello World!!";
   $color = "red";
+  $name = "Vandana Khaparde";
   echo "My car color is " . $color ;
+  echo "My name is ".$name . "!";
+  $x =5;
+  $y =8;
+  function showValues(){
+  global  $color,$x , $y;
+    echo "My favorite color is ".$color;
+    $y = $x +$y;
+    echo "$y = " .$y;
+
+  }
+  function test(){
+    $GLOBALS['x'] = $GLOBALS['x'] + $GLOBALS['y'];
+  }
+  
+  test();
+  echo $x;
+  showValues();
    ?>
